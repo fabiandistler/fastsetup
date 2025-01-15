b145d3d10ec097a4276127270ea59d54c82ce644
fabiandistler <109025423+fabiandistler@users.noreply.github.com>
2025-01-15 20:32:13 +0100

    Create install-r.sh

---
+++ install-r.sh
@@ -0,0 +1,8 @@
+`which sudo` curl -L https://rig.r-pkg.org/deb/rig.gpg -o /etc/apt/trusted.gpg.d/rig.gpg
+
+`which sudo` sh -c 'echo "deb http://rig.r-pkg.org/deb rig main" > /etc/apt/sources.list.d/rig.list'
+
+`which sudo` apt-fast update
+`which sudo` apt-fast install -yq r-rig 
+
+rig add
